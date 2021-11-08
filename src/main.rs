0e3313c4b5e5d7387a4698281af47624be80ee9c
benmdavidson <benjamindavidsonm@gmail.com>
2021-11-08 16:49:48 -0500

    11/8 in iribe

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod audio_utils;
 mod pixel_utils;
-use audio_utils::*;
-use pixel_utils::*;
-use image::io::Reader;
+use image::io::Reader as ImageReader;
+use image::open;
 use std::io::BufReader;
 use std::fs::File;
 fn main() {
@@ -10,21 +9,7 @@ fn main() {
 	// visuaudi pic=<.JPG> time=<FLOAT> name=<NAME>
 	// Example: visuaudi pic.jpg 3.2 new_audio.wav
 	// If these 3 are not specified, then fail.
-    if arguments.len() == 4 {
-        let new_img: Option<Reader<BufReader<File>>>
-            = match Reader::open(arguments[1].as_str()) {
-            Ok(r) => Some(r),
-            Err(_) => None
-        };
-        let time: Option<u32> = match arguments[2].as_str().parse::<u32>() {
-            Ok(t) => Some(t),
-            Err(_) => None,
-        };
-        // name does not need to be analyzed (probobaly)
-        // ...
-        todo!();
-    } else {
-        eprintln!("visuaudi takes 3 arguments...");
-        eprintln!("\tpic=<.JPG> time=<FLOAT> name=<NAME>");
-    }
+
+    let vec = pixel_utils::img_to_vec(String::from("ImageFiles/287585.jpg"));
+    println!("{}", vec.unwrap().len());
 }
--- src/pixel_utils.rs
+++ src/pixel_utils.rs
@@ -1,5 +1,28 @@
-pub mod pixel_utils {
-    // insert image library here...
+use image::open;
 
+pub fn img_to_vec(path: String) -> Result<Vec<u16>, &'static str> {
+
+    let file = match image::open(path) {
+        Ok(r) => Some(r),
+        Err(_) => None,
+    };
+
+    if file.is_none() {
+        return Err("Failure 1");
+    } else {
+        let rgb = file.unwrap().into_rgba();
+        let pixels = rgb.pixels();
+
+        let mut avg_pixels: Vec<u16> = Vec::with_capacity(pixels.len());
+        for px in pixels {
+            let r: u16 = px[0] as u16;
+            let g: u16 = px[1] as u16;
+            let b: u16 = px[1] as u16;
+            avg_pixels.push((r + g + b) / 3);
+        }
+
+        println!("{}", avg_pixels.len());
+        return Ok(avg_pixels);
+    }
 }
 
