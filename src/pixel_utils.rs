d8e3d812b0d75a1f2b2fb0fd750816ef95a18cf7
vibenium <vibeniumproductions@gmail.com>
2021-11-26 12:23:57 -0500

    THE FILTER WORKS

--- src/pixel_utils.rs
+++ src/pixel_utils.rs
@@ -1,5 +1,39 @@
 use image::open;
 
+fn mvn_avg_filt(num_points: usize, input_vec: &[f32]) -> Option<Vec<f32>> {
+    if num_points < input_vec.len() {
+
+        // To avoid constantly casting
+        let points: f32 = num_points as f32;
+        let mut output_vec: Vec<f32>
+            = Vec::with_capacity(input_vec.len() + num_points);
+        output_vec.push(0.0);
+
+        // Set up 1st element
+        for i in 0..num_points { // O(1)
+            output_vec[0] += input_vec[i];
+        }
+        output_vec[0] /= points;
+
+        // Applying filter...
+        for i in 1..(input_vec.len() - num_points) { // O(n)
+            output_vec.push(
+                output_vec[i - 1] -
+                (input_vec[i - 1] - input_vec[(i + num_points) - 1]) /
+                points);
+        }
+
+        // Smoothing out the last samples with exponential decay...
+        // Note that smoothing happens before end of the
+        // input vector length.
+        for i in (input_vec.len() - num_points)..output_vec.capacity() { // O(1)
+            output_vec.push(output_vec[i - 1] / points);
+        }
+        return Some(output_vec);
+    }
+    return None;
+}
+
 pub fn img_to_vec(path: String) -> Result<Vec<f32>, &'static str> {
 
     let file = match image::open(path) {
@@ -21,9 +55,13 @@ pub fn img_to_vec(path: String) -> Result<Vec<f32>, &'static str> {
             let avg: f32 = (r + g + b) / 3.0;
             avg_pixels.push((avg * (2.0/255.0) -1.0));
         }
-
+        let filtered_data = mvn_avg_filt(4, avg_pixels.as_slice());
+        match filtered_data {
+            Some(fd) => Ok(fd),
+            None => Err("Image filtered failed"),
+        }
         // println!("{}", avg_pixels.len());
-        return Ok(avg_pixels);
+        //return Ok(avg_pixels);
     }
 }
 
--- tests/tests.rs
+++ tests/tests.rs
@@ -6,6 +6,7 @@ mod test {
     use std::path::Path;
     // use image::ImageFormat;
     use image::ImageBuffer;
+    use round::round;
 
     fn mvn_avg_filt(num_points: usize, input_vec: &[f32]) -> Option<Vec<f32>> {
         if num_points < input_vec.len() {
@@ -26,12 +27,14 @@ mod test {
             for i in 1..(input_vec.len() - num_points) { // O(n)
                 output_vec.push(
                     output_vec[i - 1] -
-                    (input_vec[i - 1] + input_vec[(i + num_points) - 1]) / 
-                    points);
+                    (input_vec[i - 1] - input_vec[(i + num_points) - 1]) / 
+                    points); 
             }
            
             // Smoothing out the last samples with exponential decay...
-            for i in input_vec.len()..output_vec.len() { // O(1)
+            // Note that smoothing happens before end of the 
+            // input vector length.
+            for i in (input_vec.len() - num_points)..output_vec.capacity() { // O(1)
                 output_vec.push(output_vec[i - 1] / points);
             }
             return Some(output_vec); 
@@ -44,23 +47,87 @@ mod test {
         
         // Constructing sine wave vector
         let mut sinv: Vec<f32> = Vec::with_capacity(12);
-        for x in 0..12 { // This is roughly two cycles cuz 12 ~= 2 * (2 * pi)
+        for x in 0..sinv.capacity() { // This is roughly two cycles cuz 12 ~= 2 * (2 * pi)
             sinv.push((x as f32).sin());
         }
+        // Sanity check
+        assert_eq!(sinv.len(), 12);
         
-        let np1: usize = 3;
-        let np2: usize = 4;
-        let np3: usize = 5;
-/*
-        v1: Vec<f32> = mvn_avg_filt(np1, sinv.as_slice(), v1.as_mut_slice());
+        let np1: f32 = 3.0;
+        let np2: f32 = 4.0;
+        let np3: f32 = 5.0;
+
+        // creating the output vector
+        let mut result1: Vec<f32> = Vec::with_capacity(sinv.len() + np1 as usize);
+        for r in 0..(sinv.len() - np1 as usize) {
+            let x: f32 = (sinv[r] + sinv[r + 1] + sinv[r + 2]) / np1;
+            result1.push(x);
+        }
+        for r in (sinv.len() - np1 as usize)..result1.capacity() {
+            let x: f32 = result1[r - 1] / np1;
+            result1.push(x);
+        }
+        assert_eq!(result1.len(), 15);
+        let filter_result1: Option<Vec<f32>> = mvn_avg_filt(np1 as usize, sinv.as_slice());
+        // There are very small rounding errors
+        match filter_result1 {
+            Some(fr) => { 
+                assert_eq!(fr.len(), result1.len());
+                for r in 0..fr.len() {
+                    assert_eq!(round(fr[r] as f64, 4), round(result1[r] as f64, 4)); 
+            }},
+            None => { assert!(false); }
+        }
+
+
+
+        // creating the output vector
+        let mut result2: Vec<f32> = Vec::with_capacity(sinv.len() + np2 as usize);
+        for r in 0..(sinv.len() - np2 as usize) {
+            let x: f32 = (sinv[r] + sinv[r + 1] + sinv[r + 2] 
+                          + sinv[r + 3]) / np2;
+            result2.push(x);
+        }
+        for r in (sinv.len() - np2 as usize)..result2.capacity() {
+            let x: f32 = result2[r - 1] / np2;
+            result2.push(x);
+        }
+        assert_eq!(result2.len(), 16);
+        let filter_result2: Option<Vec<f32>> = mvn_avg_filt(np2 as usize, sinv.as_slice());
+        // There are very small rounding errors
+        match filter_result2 {
+            Some(fr) => { 
+                assert_eq!(fr.len(), result2.len());
+                for r in 0..fr.len() {
+                    assert_eq!(round(fr[r] as f64, 4), round(result2[r] as f64, 4)); 
+            }},
+            None => { assert!(false); }
+        }
        
-        // TESTING filter result...
-        for x in 0..(sinv.len() - 2) {
-            let this_avg = (sinv[x] + sinv[x + 1] + sinv[x + 2]) / 3.0;
-            println!("DEBUG: x @ {}, this_avg = {}", x, this_avg);
-            println!("WHEN v1[{}] = {}", x, v1[x]);
-            assert_eq!(v1[x], this_avg); 
-        } */
+
+        // creating the output vector
+        let mut result3: Vec<f32> = Vec::with_capacity(sinv.len() + np3 as usize);
+        for r in 0..(sinv.len() - np3 as usize) {
+            let x: f32 = (sinv[r] + sinv[r + 1] + sinv[r + 2] 
+                            + sinv[r + 3] + sinv[r + 4]) / np3;
+            result3.push(x);
+        }
+        for r in (sinv.len() - np3 as usize)..result3.capacity() {
+            let x: f32 = result3[r - 1] / np3;
+            result3.push(x);
+        }
+        assert_eq!(result3.len(), 17);
+        let filter_result3: Option<Vec<f32>> = mvn_avg_filt(np3 as usize, sinv.as_slice());
+        // There are very small rounding errors
+        match filter_result3 {
+            Some(fr) => { 
+                assert_eq!(fr.len(), result3.len());
+                for r in 0..fr.len() {
+                    assert_eq!(round(fr[r] as f64, 4), round(result3[r] as f64, 4)); 
+            }},
+            None => { assert!(false); }
+        }
+
     }
         
     #[test]
